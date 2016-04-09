3bd321e08698fc38d5af93133e1a58f6d6f666ed
HJXANDHMR <975464817@qq.com>
2016-04-09 21:15:17 +0800

    增加自定义View--CheckBoxView

--- src/main/java/com/example/hjx/androidutils/MainActivity.java
+++ src/main/java/com/example/hjx/androidutils/MainActivity.java
@@ -1,17 +1,48 @@
 package com.example.hjx.androidutils;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-import utils.SPUtil;
+import utils.NetUtil;
+import utils.view.CheckBoxView;
 
 
 public class MainActivity extends Activity {
+    private Context mContext = MainActivity.this;
+    private CheckBoxView checkBoxView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.bt_cutscreen);
+        checkBoxView = (CheckBoxView) findViewById(R.id.check1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkBoxView.isChecked()){
+                    button.setText("选中了checkBox");
+                }else{
+                    button.setText("没有选中checkBox");
+                }
+            }
+        });
+
+
+        checkBoxView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkBoxView.toggle();
+            }
+        });
+
     }
 
 }

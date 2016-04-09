8994f3b8d46a0ea553e7b6466cd3de2cae53f17d
HJXANDHMR <975464817@qq.com>
2016-04-09 18:57:07 +0800

    修改DensityUtil工具类

--- src/main/java/utils/DensityUtil.java
+++ src/main/java/utils/DensityUtil.java
@@ -20,8 +20,7 @@ public class DensityUtil {
      * @param dpVal
      * @return
      */
-    public static int dp2px(Context context, float dpVal)
-    {
+    public static int dp2px(Context context, float dpVal) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
                 dpVal, context.getResources().getDisplayMetrics());
     }

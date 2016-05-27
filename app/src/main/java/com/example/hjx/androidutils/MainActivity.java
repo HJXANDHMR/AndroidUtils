5221cb1fc2fae085821595045de489591269cdbe
hejianxiong <975464817@qq.com>
2016-05-27 15:30:12 +0800

    重构

--- app/src/main/java/com/example/hjx/androidutils/MainActivity.java
+++ app/src/main/java/com/example/hjx/androidutils/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.hjx.androidutils;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

bbd1fa69f5e5d3cbf92209ca0824eb0803da81be
hejianxiong <975464817@qq.com>
2016-06-01 21:23:26 +0800

    modify LogUti

--- app/src/main/java/utils/LogUtil.java
+++ app/src/main/java/utils/LogUtil.java
@@ -51,19 +51,19 @@ public class LogUtil {
     public static void d(String tag, String msg)
     {
         if (isDebug)
-            Log.i(tag, msg);
+            Log.d(tag, msg);
     }
 
     public static void e(String tag, String msg)
     {
         if (isDebug)
-            Log.i(tag, msg);
+            Log.e(tag, msg);
     }
 
     public static void v(String tag, String msg)
     {
         if (isDebug)
-            Log.i(tag, msg);
+            Log.v(tag, msg);
     }
 }
 
